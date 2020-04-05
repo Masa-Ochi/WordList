32ad91e11c57a06c59aca40c4255bfda84e47db2
Masa <masa.ochi.74@gmail.com>
2020-04-05 14:01:05 +0900

    Initial Commit

---
+++ WordList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordList
+//
+//  Created by Masamitsu Ochiai on 2020/04/05.
+//  Copyright © 2020 exam_org. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
